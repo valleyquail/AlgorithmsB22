4f36c7eec9490419bb7839d7024e6d1820f36c5c
Nikesh Walling <nikeshwalling@gmail.com>
2022-11-14 10:14:00 -0500

    Initial version

---
+++ src/Main.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LucasNum lucas = new LucasNum();
+        NikNum nik = new NikNum();
+        Subirachs subirach = new Subirachs();
+        int iteration = 0;
+
+//        System.out.println("Please enter the values for the Lucas Number Sequence:");
+//        int numOne = Integer.parseInt(scanner.next());
+//        int numTwo = Integer.parseInt(scanner.next());
+//        lucas.setStartNums(numOne, numTwo);
+
+
+        System.out.println("Please enter the number of iterations that you would like to calculate. Be warned, " +
+                "this takes exponentially longer to execute as the value grows.");
+        try {
+            iteration = Integer.parseInt(scanner.next());
+            System.out.println("Value accepted: " + iteration);
+        } catch (InputMismatchException e) {
+            scanner.next();
+        }
+        System.out.println("The calculated Lucas number of " + iteration + " iterations is: " +
+                lucas.calculateLucas(iteration));
+        System.out.println("Please enter another number of iterations that you would like to calculate:");
+        try {
+            iteration = Integer.parseInt(scanner.next());
+            System.out.println("Value accepted:" + iteration);
+        } catch (InputMismatchException e) {
+            scanner.next();
+        }
+        System.out.println("The ratio of growth between L(" + iteration + ") and L(" + (iteration + 1) + ")" +
+                " is: " + lucas.compareGrowth(iteration));
+        System.out.println("The ratio of time growth between L(" + iteration + ") and L(" + (iteration + 1) + ")" +
+                " is: " + lucas.compareGrowthTimes(iteration) + "\n");
+
+        //NikNum Time
+
+//        System.out.println("Please enter the values for the Nik Number Sequence:");
+//        numOne = Integer.parseInt(scanner.next());
+//        numTwo = Integer.parseInt(scanner.next());
+//        nik.setStartNums(numOne, numTwo);
+
+
+        System.out.println("Please enter the number of iterations that you would like to calculate. Be warned, " +
+                "this takes exponentially longer to execute as the value grows.");
+        try {
+            iteration = Integer.parseInt(scanner.next());
+            System.out.println("Value accepted: " + iteration);
+        } catch (InputMismatchException e) {
+            scanner.next();
+        }
+        System.out.println("The calculated Nik number of " + iteration + " iterations is: " +
+                nik.calculateNik(iteration));
+        System.out.println("Please enter another number of iterations that you would like to calculate:");
+        try {
+            iteration = Integer.parseInt(scanner.next());
+            System.out.println("Value accepted:" + iteration);
+        } catch (InputMismatchException e) {
+            scanner.next();
+        }
+        System.out.println("The ratio of growth between L(" + iteration + ") and L(" + (iteration + 1) + ")" +
+                " is: " + nik.compareGrowth(iteration));
+        System.out.println("The ratio of time growth between L(" + iteration + ") and L(" + (iteration + 1) + ")" +
+                " is: " + nik.compareGrowthTimes(iteration));
+
+    }
+}
---
+++ src/Subirachs.java
@@ -0,0 +1,31 @@
+public class Subirachs {
+
+    private final int[] subirachMatrix = {1, 14, 14, 4, 11, 7, 6, 9, 8, 10, 10, 5, 13, 2, 3, 15};
+
+    final int genericSum = 33; //1 + 14 + 14 + 4
+    final int totalSum = 132; //Sum of all the elements
+
+    public Subirachs() {
+
+    }
+
+    public int countSumsWithFour() {
+
+        return 1;
+    }
+
+    public int countSumsWithAll(){
+
+        return 1;
+    }
+
+    public int mostCommonSum(){
+        int mostCommon = 0;
+        for(int i = 0; i < 132; i++){
+
+        }
+
+        return 1;
+    }
+
+}
