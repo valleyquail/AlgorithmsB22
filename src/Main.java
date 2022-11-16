d2015123973b55c93ca53033b91b09552a98c65b
Nikesh Walling <nikeshwalling@gmail.com>
2022-11-15 21:27:56 -0500

    Working and optimized solution to the second part of the homework

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
 //        int numTwo = Integer.parseInt(scanner.next());
 //        lucas.setStartNums(numOne, numTwo);
 
-/*
+
         System.out.println("Please enter the number of iterations that you would like to calculate. Be warned, " +
                 "this takes exponentially longer to execute as the value grows.");
         try {
@@ -36,6 +36,8 @@ public class Main {
                 " is: " + lucas.compareGrowth(iteration));
         System.out.println("The ratio of time growth between L(" + iteration + ") and L(" + (iteration + 1) + ")" +
                 " is: " + lucas.compareGrowthTimes(iteration) + "\n");
+        System.out.println("The ratio is fairly close to the value of the Golden Mean = 1.618, and " +
+                "it approaches that value more as the input grows.");
 
         //NikNum Time
 
@@ -65,17 +67,19 @@ public class Main {
         System.out.println("The ratio of growth between L(" + iteration + ") and L(" + (iteration + 1) + ")" +
                 " is: " + nik.compareGrowth(iteration));
         System.out.println("The ratio of time growth between L(" + iteration + ") and L(" + (iteration + 1) + ")" +
-                " is: " + nik.compareGrowthTimes(iteration));*/
+                " is: " + nik.compareGrowthTimes(iteration));
 
         System.out.println("Moving on the the Subirachs's Square.\n The number of combinations of" +
                 " four numbers in the square that equal the column/row sum of 33 is: " +
                 subirach.countSumsWithFour());
 
-
         System.out.println("The number of combinations of any amount of numbers that sums to be 33 is: "
-                + subirach.genericSumOfAll());
+                + subirach.countGenericSum());
 
         subirach.printFinalSums();
         subirach.mostCommonSum();
+        System.out.println("As we can see, the most common sum is 66, which is twice that of 33, the magic number for" +
+                " the Subirach's Sqaure. 66 is also half the maximum sum of 132. Also, looking at the distribution, " +
+                "the sum 99 also occurs 310 times, so we can determine that the occurrences are symmetric about 66. ");
     }
 }
--- src/Subirachs.java
+++ src/Subirachs.java
@@ -3,7 +3,7 @@ public class Subirachs {
     private final int[] subirachMatrix = {1, 14, 14, 4, 11, 7, 6, 9, 8, 10, 10, 5, 13, 2, 3, 15};
     public final int genericSum = 33; //1 + 14 + 14 + 4 --> Base sum
     private final int totalSum = 132 + 1; //Sum of all the elements
-    private int[] finalSums = new int[totalSum + 1]; //Value to store the count of each possible sum plus zero
+    private int[] Sums = new int[totalSum + 1]; //Value to store the count of each possible sum plus zero
 
     public Subirachs() {
         //Nothing here lol
@@ -13,20 +13,16 @@ public class Subirachs {
     private void countSumsWithAll() {
         for (int i = 0; i < Math.pow(2, subirachMatrix.length); i++) {
             int tempSum = 0;
-            char[] temp = Integer.toBinaryString(i).toCharArray();
-            for (int j = temp.length - 1; j >= 0; j--) {
-                if (temp[j] == '1') {
-                    tempSum += subirachMatrix[temp.length - j - 1];//gross but needs in reverse the list traversal
+            int temp = i;
+            int index = 0;
+            while (temp != 0) {
+                if ((temp & 0x1) == 1) {
+                    tempSum += subirachMatrix[index];
                 }
+                index++;
+                temp >>= 1;
             }
-            finalSums[tempSum]++;
-        }
-    }
-
-    public void printFinalSums() {
-        System.out.println("The frequency of each sum is as follows:");
-        for (int i = 0; i < finalSums.length - 1; i++) {
-            System.out.println(i + " : " + finalSums[i] + "\t");
+            Sums[tempSum]++;
         }
     }
 
@@ -34,13 +30,16 @@ public class Subirachs {
     public int countSumsWithFour() {
         int sumsOfFour = 0;
         for (int i = 0; i < Math.pow(2, subirachMatrix.length); i++) {
+            int tempSum = 0;
             if (Integer.bitCount(i) == 4) {
-                int tempSum = 0;
-                char[] temp = Integer.toBinaryString(i).toCharArray();
-                for (int j = temp.length - 1; j >= 0; j--) {
-                    if (temp[j] == '1') {
-                        tempSum += subirachMatrix[temp.length - j - 1];//gross but needed for list reverse traversal
+                int temp = i;
+                int index = 0;
+                while (temp != 0) {
+                    if ((temp & 0x1) == 1) {
+                        tempSum += subirachMatrix[index];
                     }
+                    index++;
+                    temp >>= 1;
                 }
                 if (tempSum == genericSum) sumsOfFour++;
             }
@@ -48,39 +47,27 @@ public class Subirachs {
         return sumsOfFour;
     }
 
-//    public int countSums() {
-//        int sumOfGeneric = 0;
-//        for (int i = 0; i < Math.pow(2, 16); i++) {
-//            int tempSum = 0;
-//            char[] temp = Integer.toBinaryString(i).toCharArray();
-//            for (int j = 0; j < 15; j++) {
-//                if (temp[i] == '1') {
-//                    tempSum += subirachMatrix[j];
-//                }
-//                if (tempSum == genericSum) sumOfGeneric++;
-//            }
-//        }
-//        return sumOfGeneric;
-//    }
+    public int countGenericSum() {
+        return Sums[genericSum];
+    }
 
-    //Serves to answer both questions 2 and 3 at the same time, and then other functions return the values
-
-    public int genericSumOfAll() {
-        return finalSums[genericSum];
+    public void printFinalSums() {
+        System.out.println("The frequency of each sum is as follows:");
+        for (int i = 0; i < Sums.length - 1; i++) {
+            System.out.println(i + " : " + Sums[i] + "\t");
+        }
     }
 
     public void mostCommonSum() {
-        int mostCommon = finalSums[0];
+        int mostCommon = Sums[0];
         int temp = 0;
-        for (int i = 0; i < finalSums.length -1; i++) {
-            if (finalSums[i] > mostCommon) {
-                mostCommon = finalSums[i];
+        for (int i = 0; i < Sums.length - 1; i++) {
+            if (Sums[i] > mostCommon) {
+                mostCommon = Sums[i];
                 temp = i;
             }
         }
         System.out.println("The most common sum using all combinations is " + temp
-                + " with " + finalSums[temp]+ " combinations.");
+                + " with " + Sums[temp] + " combinations.");
     }
-
-
 }
